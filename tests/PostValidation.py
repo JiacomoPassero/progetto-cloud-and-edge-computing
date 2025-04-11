973917f09a40f39e8cecd52cf17a0d1b02045e8e
Umberto Paroli <buparoli9@gmail.com>
2025-04-11 07:40:53 +0200

    aggiunta funzione per estrarre il valore di un campo in formato stringa

--- tests/PostValidation.py
+++ tests/PostValidation.py
@@ -1,9 +1,9 @@
 import unittest
 import re
 import os
-"""Fnctuon to extract the template from an header file"""
-def extract_fields(post_path, separator):
-    #Extract post structure from template file
+
+"""Support Function to extract the text of the header"""
+def extract_header_text(post_path, separator):
     with open(post_path, 'r') as template_file:
         header_text = ""
         #Extract lines unitl "---" that marks the end of the header
@@ -12,11 +12,17 @@ def extract_fields(post_path, separator):
                 break
             else:
                 header_text += line
+    return header_text
 
-        #Using regex to extract only the fileds name
-        field_pattern = re.compile(r'^(\w+): .+', re.MULTILINE)
-        required_fields = field_pattern.findall(header_text)
-        return required_fields
+"""Functuon to extract the template from an header file"""
+def extract_fields(post_path, separator):
+    #Extract post structure from template file
+    header_text = extract_header_text(post_path, separator)
+
+    #Using regex to extract only the fileds name
+    field_pattern = re.compile(r'^(\w+): .+', re.MULTILINE)
+    required_fields = field_pattern.findall(header_text)
+    return required_fields
 
 """Check if the post contains the separator line between the header and the body"""
 def contains_separator(post_path, separator):
@@ -28,8 +34,18 @@ def contains_separator(post_path, separator):
             #separator is a single line
             if separator + "\n" == line:
                 return True
-
     return False
+
+"""Extraxt the string value of a field to be checked inside the test"""
+def extract_field_value(post_path, field):
+    #extract the jeader
+    header_text = extract_header_text(post_path, separator)
+    #build abd apply the regex
+    regex = f'^{field}: .+'
+    field_pattern = re.compile(regex, re.MULTILINE)
+    field_line = field_pattern.findall(header_text)
+    #return only the value of the line stripped from excessive spaces
+    return field_line[0].split(":")[1].strip()
 
 
 class PostValidator(unittest.TestCase):
@@ -54,6 +70,9 @@ class PostValidator(unittest.TestCase):
             #extract and compare the post header
             post_fields = extract_fields(os.path.join(self.posts_path, post), self.separator) 
             self.assertEqual(post_fields, template_fields, f"Post {post} does not match the template header structure")
+            
+            #check date format
+            date = extract_field_value(post_path, "date")
 
 
 
